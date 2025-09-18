883fe97b8e6dc2947ccc8ea8af4b41e102d123c2
Meekness-zw <mikmarkson@gmail.com>
2025-09-18 15:28:23 +0200

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://yourdomain.com', // <-- replace with your real domain
+    generateRobotsTxt: true, // generates robots.txt as well
+    sitemapSize: 7000, // optional, default 5000
+  };
+  
